000a4a2d1bd17a07fece92d2affd7d0c7ea4a436
Joy <neloy1613@gmail.com>
2019-11-27 20:26:55 +0600

    App modified

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const app = express();
 // parse requests
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const config = require('./config.js');
 const mongoose = require('mongoose');
+require('./product.routes.js')(app);  //Add route file here
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect(config.url, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+	 useUnifiedTopology: true 
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
@@ -26,7 +34,9 @@ mongoose.connect(config.url, {
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to ZeptoBook Product app"});
 });
-app.listen(config.serverport, () => {
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 })
 
