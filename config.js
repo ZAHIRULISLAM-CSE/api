d99a902f3c56fe0cfefead8917e1346e2ecb0012
ZAHIRULISLAM-CSE <48257712+ZAHIRULISLAM-CSE@users.noreply.github.com>
2019-11-27 13:37:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     url: 'mongodb+srv://joy:123456seven@mongo12-g2e4l.mongodb.net/test?retryWrites=true&w=majority',
-    serverport: 5001
+    serverport: 5001 || process.env.PORT
 }
